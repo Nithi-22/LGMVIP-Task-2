654318ad0ba5e3131f5626055e16842b36299867
Nithi-22 <90109517+Nithi-22@users.noreply.github.com>
2021-10-21 19:44:38 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,49 @@
+import Users from "./components/Details";
+import "./styles.css";
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { users_data: [], loading: true };
+    this.showUsers = this.showUsers.bind(this);
+  }
+
+  showUsers() {
+    document.getElementById("MainBlock").style.display = "flex";
+    const source = "https://reqres.in/api/users?page=1";
+    fetch(source)
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ users_data: users.data, loading: false });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <nav className="navbar navbar-expand-lg navbar-dark  m-3">
+          <div className="container">
+            <h3><center>NITHI SNEHA S</center></h3>
+            <div className="sliding">
+              
+
+            < h3 ><center>Info: Kindly click  Get Users to get the details</center></h3>
+            </div>
+            </div>
+            <div className="c">
+            <button onClick={this.showUsers}>Get Users</button>
+          
+          </div>
+        </nav>
+
+        <Users loading={this.state.loading} users={this.state.users_data} />
+      </>
+    );
+  }
+}
+
+export default App;
